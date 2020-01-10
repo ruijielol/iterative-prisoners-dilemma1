2c0132ef385553db87564de67f45ab01cd2db339
ivanyeddu <59658487+ivanyeddu@users.noreply.github.com>
2020-01-10 11:15:27 -0500

    Revert "Update team10.py"
    This reverts commit 64e1de0ebc26783a54f16424f4ac9f1b962de805.

--- team10.py
+++ team10.py
@@ -26,11 +26,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    count = 0
-    if count % 2 == 1:
-        return 'c'
-    else:
-        return 'b'
+    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
